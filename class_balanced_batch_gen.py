61a79b4084d1f58715d1e7d3becbd281824a6162
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-18 17:37:54 +0800

    Fix customized generator

--- class_balanced_batch_gen.py
+++ class_balanced_batch_gen.py
@@ -1,12 +1,24 @@
 import random
 import numpy as np
+from tensorflow.python.keras.utils.data_utils import Sequence
 
 
-class ClassBalancedBatchGenerator():
+class ClassBalancedBatchGenerator(Sequence):
   def __init__(self, X_train, y_train, batch_size):
     self.X_train = X_train
     self.y_train = y_train
     self.batch_size = batch_size
+    self.generator = self.train_generator(batch_size=batch_size)
+
+  def __len__(self):
+    return np.floor(self.X_train.shape[0] / self.batch_size)
+
+  @property
+  def shape(self):
+    return self.X_train.shape
+
+  def next(self):
+    return self.__next__()
 
   def get_rearranged_batch_indices(self, X_train, y_train):
     num_instance_for_each_class = int(len(X_train) / 100)
@@ -34,9 +46,12 @@ class ClassBalancedBatchGenerator():
                           batch_size:(current_batch_id + 1) *
                           batch_size]
 
-  def train_generator(batch_size=1000):
+  def train_generator(self, batch_size=1000):
     batch_indices_gen = self.batch_indice_generator(
         self.X_train, self.y_train, batch_size=self.batch_size)
     while True:
       batch_indices = next(batch_indices_gen)
       yield self.X_train[batch_indices], self.y_train[batch_indices]
+
+  def __next__(self):
+    return next(self.generator)
