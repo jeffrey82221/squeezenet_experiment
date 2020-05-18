8b57739e4ffcda4969217f7ff7fe9d77d25201b9
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-18 18:28:19 +0800

    Fig bug

--- class_balanced_batch_gen.py
+++ class_balanced_batch_gen.py
@@ -1,6 +1,14 @@
 import random
 import numpy as np
 from tensorflow.python.keras.utils.data_utils import Sequence
+'''
+from class_balanced_batch_gen import ClassBalancedBatchGenerator
+from tensorflow.keras.datasets import cifar100
+import pickle
+(X_train, y_train), (X_test, y_test) = cifar100.load_data()
+gen = ClassBalancedBatchGenerator(X_train, y_train, 1000)
+gen.get_rearranged_batch_indices(X_train, y_train)
+'''
 
 
 class ClassBalancedBatchGenerator(Sequence):
@@ -8,6 +16,7 @@ class ClassBalancedBatchGenerator(Sequence):
     self.X_train = X_train
     self.y_train = y_train
     self.batch_size = batch_size
+    assert self.batch_size % 100 == 0
     self.generator = self.train_generator(batch_size=batch_size)
 
   def __len__(self):
@@ -17,13 +26,16 @@ class ClassBalancedBatchGenerator(Sequence):
   def shape(self):
     return self.X_train.shape
 
-  def next(self):
+  def __getitem__(self, item):
     return self.__next__()
 
-  def get_rearranged_batch_indices(self, X_train, y_train):
-    num_instance_for_each_class = int(len(X_train) / 100)
+  def __next__(self):
+    return next(self.generator)
+
+  def get_rearranged_batch_indices(self):
+    num_instance_for_each_class = int(len(self.X_train) / 100)
     indices_for_each_class = [
-        np.where(y_train == c)[0] for c in range(100)
+        np.where(self.y_train == c)[0] for c in range(100)
     ]
     for c in range(100):
       random.shuffle(indices_for_each_class[c])
@@ -32,26 +44,20 @@ class ClassBalancedBatchGenerator(Sequence):
       for c in range(100):
         new_indices.append(indices_for_each_class[c][i])
     return new_indices
-    # X_train = X_train[new_indices]
-    # y_train = y_train[new_indices]
 
-  def batch_indice_generator(self, X_train, y_train, batch_size=1000):
-    assert batch_size % 100 == 0
-    train_data_num = len(X_train)
-    batch_count = int(train_data_num / batch_size)
+  def batch_indice_generator(self):
+
+    train_data_num = len(self.X_train)
+    batch_count = int(train_data_num / self.batch_size)
     while True:
-      new_indices = self.get_rearranged_batch_indices(X_train, y_train)
+      new_indices = self.get_rearranged_batch_indices()
       for current_batch_id in range(batch_count):
         yield new_indices[current_batch_id *
-                          batch_size:(current_batch_id + 1) *
-                          batch_size]
+                          self.batch_size:(current_batch_id + 1) *
+                          self.batch_size]
 
-  def train_generator(self, batch_size=1000):
-    batch_indices_gen = self.batch_indice_generator(
-        self.X_train, self.y_train, batch_size=self.batch_size)
+  def train_generator(self):
+    batch_indices_gen = self.batch_indice_generator()
     while True:
       batch_indices = next(batch_indices_gen)
       yield self.X_train[batch_indices], self.y_train[batch_indices]
-
-  def __next__(self):
-    return next(self.generator)
