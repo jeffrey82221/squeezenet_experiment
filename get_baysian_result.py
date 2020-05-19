967f180dc27644fdfee45be6a8f8d5bb33c3231d
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-19 09:50:37 +0800

    modulize get_baysian_result

--- get_baysian_result.py
+++ get_baysian_result.py
@@ -2,21 +2,26 @@ from OneCycleTrainFunc import OneCycleTrain
 from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
 from bayes_opt.util import load_logs
-pbounds = {
-    'squeeze_scale_exp': (1.5, 1.5),  # 2
-    'small_filter_rate': (0.5, 0.5),  # 10
-    'max_lr_exp': (-4, -2),  # 6
-    'max_momentum': (0.8, 0.99),
-    'num_epoch': (20, 50)
-}
-optimizer = BayesianOptimization(
-    f=OneCycleTrain,
-    pbounds=pbounds,
-    random_state=1,
-)
-load_logs(optimizer, logs=["./one_cycle_baysian_logs.json"])
 
-for i, res in enumerate(optimizer.res):
-    print("Iteration {}: \n\t{}".format(i, res))
 
-print("Final Max:", optimizer.max)
+def get():
+    pbounds = {
+        'squeeze_scale_exp': (1.5, 1.5),  # 2
+        'small_filter_rate': (0.5, 0.5),  # 10
+        'max_lr_exp': (-4, -2),  # 6
+        'max_momentum': (0.8, 0.99),
+        'num_epoch': (20, 50)
+    }
+    optimizer = BayesianOptimization(
+        f=OneCycleTrain,
+        pbounds=pbounds
+    )
+    load_logs(optimizer, logs=["./one_cycle_baysian_logs.json"])
+
+    res_list = []
+    for i, res in enumerate(optimizer.res):
+        print("Iteration {}: \n\t{}".format(i, res))
+        res_list.append(res)
+
+    # print("Final Max:", optimizer.max)
+    return res_list
