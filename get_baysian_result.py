4faaa2db7c1546b8bfcabe3b583fb91ec95528ae
jeffrey82221 <jeffrey82221@gmail.com>
2022-02-27 20:52:04 +0800

    [update] files

--- get_baysian_result.py
+++ get_baysian_result.py
@@ -3,6 +3,19 @@ from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
 from bayes_opt.util import load_logs
 
+'''
+from get_baysian_result import get
+data = get()
+for v in list(map(lambda x:x['target'],data)):
+    print(v)
+for v in list(map(lambda x:x['params']['max_lr_exp'],data)):
+    print(v)
+for v in list(map(lambda x:x['params']['max_momentum'],data)):
+    print(v)
+for v in list(map(lambda x:x['params']['num_epoch'],data)):
+    print(v)
+ '''
+
 
 def get():
     pbounds = {
